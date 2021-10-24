e10a34cf6085ca09b44057b721a09f180b153c12
lykmapipo <lallyelias87@gmail.com>
2021-10-24 10:06:45 +0300

    chore: add sources directory structure

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from .data import *  # noqa
+from .features import *  # noqa
+from .models import *  # noqa
+from .visualization import *  # noqa
---
+++ src/data/README.md
@@ -0,0 +1,1 @@
+Scripts to download or generate data.
